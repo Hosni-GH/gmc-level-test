ea04072e60dcf537d593b9c18be567486d3c0089
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-30 16:39:07 +0100

    Updates

---
+++ precache-manifest.76c780bf8ff14163f1114761f555be03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a465a34224f728f22d6d7351c99bc82",
+    "url": "/gmc-level-test/index.html"
+  },
+  {
+    "revision": "74804a86109356a028de",
+    "url": "/gmc-level-test/static/css/main.b4cdbe0e.chunk.css"
+  },
+  {
+    "revision": "d9cdc1324cce01c03c73",
+    "url": "/gmc-level-test/static/js/2.a44e6354.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/gmc-level-test/static/js/2.a44e6354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74804a86109356a028de",
+    "url": "/gmc-level-test/static/js/main.d6f681f3.chunk.js"
+  },
+  {
+    "revision": "667b82c414af7b9be3c8",
+    "url": "/gmc-level-test/static/js/runtime-main.46f2adab.js"
+  }
+]);
